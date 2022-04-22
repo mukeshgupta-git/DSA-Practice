bc3a091fa8955dc4a3b3d95241b36da531c442c8
mukeshgupta-coder <eng.mukeshgupta@gmail.com>
2022-04-22 09:34:31 +0530

    ArrayListTest Example Test

---
+++ src/com/practice/dsa/list/ArrayListTest.java
@@ -0,0 +1,14 @@
+package com.practice.dsa.list;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(34);
+		System.out.println(list);
+
+	}
+
+}
