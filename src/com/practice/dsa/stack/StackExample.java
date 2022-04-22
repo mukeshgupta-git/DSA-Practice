62073b72c244d6860bac6c7e42723ad9b7192125
mukeshgupta-coder <eng.mukeshgupta@gmail.com>
2022-04-22 09:39:19 +0530

    Stack examples using java collection

---
+++ src/com/practice/dsa/stack/StackExample.java
@@ -0,0 +1,15 @@
+package com.practice.dsa.stack;
+
+import java.util.Stack;
+
+public class StackExample {
+
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<>();
+		stack.add(34);
+		stack.add(55);
+		System.out.println(stack);
+
+	}
+
+}
